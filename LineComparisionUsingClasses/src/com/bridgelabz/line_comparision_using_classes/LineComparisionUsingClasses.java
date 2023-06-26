7f3d6ad50d0b93b67f632af55e8f79b5c2c16ca5
Prince Kumar <kumarprince1407@gmail.com>
2023-06-27 01:04:37 +0530

    Added Use Case 2

--- LineComparisionUsingClasses/src/com/bridgelabz/line_comparision_using_classes/LineComparisionUsingClasses.java
+++ LineComparisionUsingClasses/src/com/bridgelabz/line_comparision_using_classes/LineComparisionUsingClasses.java
@@ -17,5 +17,23 @@ public class LineComparisionUsingClasses {
 
         double l1 = (Math.sqrt(Math.pow((x2 - x1),2) )+ Math.pow((y2 -y1),2));
         System.out.println("The length of the line L1 is: "+ l1);
+        System.out.println("Enter the coordinates of Line 2.");
+        System.out.print("Enter the value of x1:");
+        double x3 = sc.nextDouble();
+        System.out.print("Enter the value of x2:");
+        double x4 = sc.nextDouble();
+        System.out.print("Enter the value of y1:");
+        double y3 = sc.nextDouble();
+        System.out.print("Enter the value of y2:");
+        double y4 = sc.nextDouble();
+        double l2 = (Math.sqrt(Math.pow((x4 - x3),2) )+ Math.pow((y4 -y3),2));
+        System.out.println("The length of the line L2 is: "+ l2);
+
+        double compare = Double.compare(l1,l2);
+        if(compare>0){
+            System.out.println("The length of line L1 is greater than L2.");
+        }else {
+            System.out.println("The length of line L2 is greater than L1.");
+        }
     }
 }
