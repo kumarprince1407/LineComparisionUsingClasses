9b16d008a1fd88cdca5c6b3823d9a964de03d586
Prince Kumar <kumarprince1407@gmail.com>
2023-06-27 00:49:46 +0530

    Added initial commit

---
+++ LineComparisionUsingClasses/src/com/bridgelabz/line_comparision_using_classes/LineComparisionUsingClasses.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.line_comparision_using_classes;
+
+public class LineComparisionUsingClasses {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison computation program! ");
+    }
+}
